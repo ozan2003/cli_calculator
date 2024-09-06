f5e03f846054d9fa2aea694eb1a02fa5e17b219e
Ozan Malci <ozan41094@gmail.com>
2024-09-06 14:04:45 +0300

    Adding test cases.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,230 +1,51 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using Calculator;
 
-namespace Calculator;
+namespace Program;
 
-internal class Calculator
+public class Program
 {
-    private static readonly Dictionary<char, Operator> @operator = new()
+    public static void Test(Dictionary<string, double> tests)
     {
-        {'(', new('(', 4, Associavity.Left)},
-        {')', new(')', 4, Associavity.Left)},
-        {'+', new('+', 1, Associavity.Left)},
-        {'-', new('-', 1, Associavity.Left)},
-        {'*', new('*', 2, Associavity.Left)},
-        {'/', new('/', 2, Associavity.Left)},
-        {'^', new('^', 3, Associavity.Right)},
-    };
-
-    /// <summary>
-    /// Check the character is an operator or not.
-    /// </summary>
-    /// 
-    /// <param name="ch">
-    /// The character to check.
-    /// </param>
-    /// 
-    /// <returns>
-    /// The corresponding operator if the character is an operator, null otherwise.
-    /// </returns>
-    private static Operator? IsOperator(in char ch) =>
-        @operator.TryGetValue(ch, out Operator? op) ? op : null;
-   
-
-    /// <summary>
-    /// Converts the infix expression to postfix notation.
-    /// </summary>
-    /// 
-    /// <param name="input">
-    /// The infix expression to convert.
-    /// </param>
-    /// 
-    /// <returns>
-    /// The postfix notation of the input expression.
-    /// </returns>
-    ///
-    /// <exception cref="ArgumentException">
-    /// If there are invalid characters in the infix expression.
-    /// i.e. characters other than digits, arithmetic operators, and parentheses.
-    /// </exception>
-    private static string ToPostfix(in string input)
-    {
-        // Remove all whitespaces and convert to a list of characters.
-        StringBuilder infix = new(input.Length + 1); // +1 for the possible extra '0' at the beginning.
-
-        if (input.StartsWith('-'))
+        foreach (var (test, result) in tests)
         {
-            infix.Append('0');
-        }
-        // Handle negative numbers at the beginning of the expression.
-        infix.Append(string.Concat(input.Where(ch => !char.IsWhiteSpace(ch))));
-
-        //---------------------------------------------------------------------------
-        Stack<Operator> operator_stack = new();
-        StringBuilder postfix = new(infix.Length);
-
-        for (int i = 0; i < infix.Length; ++i)
-        {
-            // Handle numbers (multi-digit).
-            if (char.IsDigit(infix[i]))
+            var actual_result = Calculator.Calculator.Calculate(test);
+            if (actual_result != result)
             {
-                StringBuilder number = new();
-
-                // Capture the entire number (multi-digit support)
-                while (i < infix.Length && char.IsDigit(infix[i]))
-                {
-                    number.Append(infix[i]);
-                    ++i;
-                }
-
-                postfix.Append(number);
-                postfix.Append(' ');
-                --i; // Adjust to correct the loop increment.
-            }
-            // Handle numbers (negative).
-            // Whenever '-' comes in string, check if there's a number before it.
-            // If not push '0' then push '-'.
-            else if (infix[i] == '-' && (i == 0 || infix[i - 1] == '('))
-            {
-                postfix.Append("0 ");
-                //operator_stack.Push(infix[i]);
-                operator_stack.Push(@operator['-']);
-            }
-            // If it's an operator.
-            else if (IsOperator(infix[i]) is var opr && opr is not null)
-            {
-                while (operator_stack.Count != 0 && operator_stack.Peek().Priority >= opr.Priority)
-                {
-                    postfix.Append(operator_stack.Pop().Opr);
-                    postfix.Append(' ');
-                }
-                operator_stack.Push(opr);
-            }
-#if false
-            else if (IsOperator(infix[i]))
-            {
-                while (
-                    operators.Count != 0
-                    && GetPriority(operators.Peek()) >= GetPriority(infix[i])
-                )
-                {
-                    postfix.Append(operators.Pop());
-                    postfix.Append(' ');
-                }
-                operators.Push(infix[i]);
-            }
-#endif
-            // Opening parenthesis
-            else if (infix[i] == '(')
-            {
-                //operator_stack.Push(infix[i]);
-                operator_stack.Push(@operator['(']);
-            }
-            // Closing parenthesis
-            else if (infix[i] == ')')
-            {
-                while (operator_stack.Count != 0 && operator_stack.Peek().Opr != '(')
-                {
-                    postfix.Append(operator_stack.Pop().Opr);
-                    postfix.Append(' ');
-                }
-                operator_stack.Pop(); // Remove '(' from stack
-            }
-#if false
-            else if (infix[i] == ')')
-            {
-                while (operator_stack.Count != 0 && operator_stack.Peek() != '(')
-                {
-                    postfix.Append(operator_stack.Pop());
-                    postfix.Append(' ');
-                }
-                operator_stack.Pop(); // Remove '(' from stack
-            }
-#endif
-            // Whitespace
-            else if (char.IsWhiteSpace(infix[i]))
-            {
-                continue;
+                Console.WriteLine(
+                    "Test failed for '{0}'.\nActual result: {1}\nExpected: {2}",
+                    test,
+                    result,
+                    actual_result
+                );
             }
             else
             {
-                throw new ArgumentException(
-                    $"Invalid character '{infix[i]}' in the infix expression."
-                );
+                Console.WriteLine("Passed!");
             }
         }
-
-        // Pop any remaining operators
-        while (operator_stack.Count != 0)
-        {
-            postfix.Append(operator_stack.Pop());
-            postfix.Append(' ');
-        }
-
-        return postfix.ToString().TrimEnd();
     }
 
-    /// <summary>
-    /// Evaluates RPN (Reverse Polish Notation) expression.
-    /// </summary>
-    /// 
-    /// <param name="infix">
-    /// The infix expression to evaluate.
-    /// </param>
-    /// 
-    /// <returns>
-    /// The result of the expression.
-    /// </returns>
-    /// 
-    /// <exception cref="ArgumentException">
-    /// If there are invalid characters in the postfix expression.
-    /// i.e. characters other than digits, arithmetic operators, and spaces.
-    /// </exception>
-    public static double Calculate(in string infix)
+    public static void Main()
     {
-        Stack<double> nums = new();
-        string postfix = ToPostfix(infix);
-
-        foreach (string token in postfix.Split(' '))
-        {
-            char front = token[0];
-
-            if (char.IsDigit(front) || (front == '-' && token.Length > 1))
+        Dictionary<string, double> testcases =
+            new()
             {
-                nums.Push(Convert.ToDouble(token));
-            }
-            else if (IsOperator(front) && nums.Count >= 2)
-            {
-                var second = nums.Pop();
-                var first = nums.Pop();
+                { "3+5-(2+6)*3", -16.0 },
+                { "24 - 32 + 29 * 3 / (3 - 9)", -22.5 },
+                { "2^3", 8 },
+                { "2^3 + 10", 18 },
+                { "2 ^ 3 + (5 * 3) / 2", 15.5 },
+                { "(1+(4+5+2)-3)+(6 ^ 8)", 1_679_625.0 },
+                { "- (3 + (4 + 5))", -12.0 },
+                { "1-(     -2)", 3.0 },
+                { "1 + 1", 2.0 },
+                { " 2-1 + 2 ", 3.0 },
+                { "- (3 - (- (4 + 5) ) )", -12.0 },
+                { "14 - (4 * 8 - 60) / 9", 14 - (4 * 8 - 60) / 9.0 },
+            };
 
-                switch (front)
-                {
-                    case '+':
-                        nums.Push(first + second);
-                        break;
-                    case '-':
-                        nums.Push(first - second);
-                        break;
-                    case '*':
-                        nums.Push(first * second);
-                        break;
-                    case '/':
-                        nums.Push(first / second);
-                        break;
-                    default:
-                        break;
-                }
-            }
-            else
-            {
-                throw new ArgumentException(
-                    $"Invalid character '{front}' in the postfix expression."
-                );
-            }
-        }
-
-        return nums.Peek();
+        Test(testcases);
     }
 }
