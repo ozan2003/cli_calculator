baccf366292fd1cfec37e21e291a9b7bdf81b596
Ozan Malci <ozan41094@gmail.com>
2024-09-07 09:54:24 +0300

    Add exception handling and new test cases in Program
    The `Test` method now includes exception handling to manage potential errors during the calculation process. Improved readability of the output messages for both failed and passed tests. Updated and added new test cases in the `Main` method to ensure comprehensive testing of the calculator functionality.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,20 +6,29 @@ public class Program
     {
         foreach (var (test, result) in tests)
         {
-            var actual_result = Calculator.Calculator.Calculate(test);
-            if (actual_result != result)
+            try
             {
-                Console.WriteLine(
-                    "Test failed for '{0}'.\nActual result: {1}\nExpected: {2}",
-                    test,
-                    result,
-                    actual_result
-                );
+                var actual_result = Calculator.Calculator.Calculate(test);
+                if (actual_result != result)
+                {
+                    Console.WriteLine(
+                        "\nTest failed for '{0}'.\nActual result: {1}\nExpected: {2}\n",
+                        test,
+                        result,
+                        actual_result
+                    );
+                }
+                else
+                {
+                    Console.WriteLine("Passed!");
+                }
             }
-            else
+            catch (Exception)
             {
-                Console.WriteLine("Passed!");
+                Console.WriteLine($"Exception happened with '{test}'.");
+                continue;
             }
+
         }
     }
 
@@ -28,6 +37,8 @@ public class Program
         Dictionary<string, double> testcases =
             new()
             {
+                { "1-(     -2)", 3.0 },
+                { "1+2*-4--3", -10.0},
                 { "3+5-(2+6)*3", -16.0 },
                 { "24 - 32 + 29 * 3 / (3 - 9)", -22.5 },
                 { "2^3", 8 },
@@ -35,11 +46,11 @@ public class Program
                 { "2 ^ 3 + (5 * 3) / 2", 15.5 },
                 { "(1+(4+5+2)-3)+(6 ^ 8)", 1_679_625.0 },
                 { "- (3 + (4 + 5))", -12.0 },
-                { "1-(     -2)", 3.0 },
                 { "1 + 1", 2.0 },
                 { " 2-1 + 2 ", 3.0 },
                 { "- (3 - (- (4 + 5) ) )", -12.0 },
                 { "14 - (4 * 8 - 60) / 9", 14 - (4 * 8 - 60) / 9.0 },
+                { "1 + 5 * (-6/3)", -9.0}
             };
 
         Test(testcases);
