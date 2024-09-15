ec392deea6cd4d87a689f3370ec75218f5381691
Ozan Malci <ozan41094@gmail.com>
2024-09-15 15:14:35 +0300

    Adding main logic of the CLI app.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,58 +1,33 @@
-﻿namespace Program;
+﻿namespace Calculator;
 
 public class Program
 {
-    public static void Test(Dictionary<string, double> tests)
-    {
-        foreach (var (test, result) in tests)
-        {
-            try
-            {
-                var actual_result = Calculator.Calculator.Calculate(test);
-                if (actual_result != result)
-                {
-                    Console.WriteLine(
-                        "\nTest failed for '{0}'.\nActual result: {1}\nExpected: {2}\n",
-                        test,
-                        result,
-                        actual_result
-                    );
-                }
-                else
-                {
-                    Console.WriteLine("Passed!");
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine($"Exception happened with '{test}'.");
-                continue;
-            }
-
-        }
-    }
-
     public static void Main()
     {
-        Dictionary<string, double> testcases =
-            new()
-            {
-                { "1-(     -2)", 3.0 },
-                { "1+2*-4--3", -10.0},
-                { "3+5-(2+6)*3", -16.0 },
-                { "24 - 32 + 29 * 3 / (3 - 9)", -22.5 },
-                { "2^3", 8 },
-                { "2^3 + 10", 18 },
-                { "2 ^ 3 + (5 * 3) / 2", 15.5 },
-                { "(1+(4+5+2)-3)+(6 ^ 8)", 1_679_625.0 },
-                { "- (3 + (4 + 5))", -12.0 },
-                { "1 + 1", 2.0 },
-                { " 2-1 + 2 ", 3.0 },
-                { "- (3 - (- (4 + 5) ) )", -12.0 },
-                { "14 - (4 * 8 - 60) / 9", 14 - (4 * 8 - 60) / 9.0 },
-                { "1 + 5 * (-6/3)", -9.0}
-            };
+        Console.WriteLine("Type an expression to calculate or 'exit' to quit.");
+        while (true)
+        {
+            Console.Write("> ");
 
-        Test(testcases);
+            string input = Console.ReadLine() ?? string.Empty;
+
+            switch (input)
+            {
+                case "":
+                    continue;
+                case "exit":
+                    return;
+                default:
+                    try
+                    {
+                        Console.WriteLine(Calculator.Calculate(input));
+                    }
+                    catch (Exception exc)
+                    {
+                        Console.WriteLine(exc.Message);
+                    }
+                    break;
+            }
+        }
     }
 }
