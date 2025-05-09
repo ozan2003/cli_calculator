0f00897d2c682105365c9ce8ab2b4a2cab23dcb7
Ozan Malci <ozan41094@gmail.com>
2025-05-09 17:24:01 +0300

    Remove Calculator and Program files, consolidating functionality into unit tests. Updated unit tests to directly reference the Calculator class for improved clarity and maintainability.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -5,25 +5,27 @@ namespace Tests;
 [TestClass]
 public class UnitTests
 {
+    private readonly Calculator.Calculator _calculator = new();
+
     [TestMethod]
     public void TestCalculate()
     {
-        Assert.AreEqual(3.0, Calculator.Calculator.Calculate("1-(     -2)"));
-        Assert.AreEqual(-4.0, Calculator.Calculator.Calculate("1+2*-4--3"));
-        Assert.AreEqual(-16.0, Calculator.Calculator.Calculate("3+5-(2+6)*3"));
-        Assert.AreEqual(-22.5, Calculator.Calculator.Calculate("24 - 32 + 29 * 3 / (3 - 9)"));
-        Assert.AreEqual(8, Calculator.Calculator.Calculate("2^3"));
-        Assert.AreEqual(18, Calculator.Calculator.Calculate("2^3 + 10"));
-        Assert.AreEqual(15.5, Calculator.Calculator.Calculate("2 ^ 3 + (5 * 3) / 2"));
-        Assert.AreEqual(1_679_625.0, Calculator.Calculator.Calculate("(1+(4+5+2)-3)+(6 ^ 8)"));
-        Assert.AreEqual(-12.0, Calculator.Calculator.Calculate("- (3 + (4 + 5))"));
-        Assert.AreEqual(2.0, Calculator.Calculator.Calculate("1 + 1"));
-        Assert.AreEqual(3.0, Calculator.Calculator.Calculate(" 2-1 + 2 "));
-        Assert.AreEqual(-12.0, Calculator.Calculator.Calculate("- (3 - (- (4 + 5) ) )"));
+        Assert.AreEqual(3.0, _calculator.Calculate("1-(     -2)"));
+        Assert.AreEqual(-4.0, _calculator.Calculate("1+2*-4--3"));
+        Assert.AreEqual(-16.0, _calculator.Calculate("3+5-(2+6)*3"));
+        Assert.AreEqual(-22.5, _calculator.Calculate("24 - 32 + 29 * 3 / (3 - 9)"));
+        Assert.AreEqual(8, _calculator.Calculate("2^3"));
+        Assert.AreEqual(18, _calculator.Calculate("2^3 + 10"));
+        Assert.AreEqual(15.5, _calculator.Calculate("2 ^ 3 + (5 * 3) / 2"));
+        Assert.AreEqual(1_679_625.0, _calculator.Calculate("(1+(4+5+2)-3)+(6 ^ 8)"));
+        Assert.AreEqual(-12.0, _calculator.Calculate("- (3 + (4 + 5))"));
+        Assert.AreEqual(2.0, _calculator.Calculate("1 + 1"));
+        Assert.AreEqual(3.0, _calculator.Calculate(" 2-1 + 2 "));
+        Assert.AreEqual(-12.0, _calculator.Calculate("- (3 - (- (4 + 5) ) )"));
         Assert.AreEqual(
             14 - (4 * 8 - 60) / 9.0,
-            Calculator.Calculator.Calculate("14 - (4 * 8 - 60) / 9")
+            _calculator.Calculate("14 - (4 * 8 - 60) / 9")
         );
-        Assert.AreEqual(-9.0, Calculator.Calculator.Calculate("1 + 5 * (-6/3)"));
+        Assert.AreEqual(-9.0, _calculator.Calculate("1 + 5 * (-6/3)"));
     }
 }
